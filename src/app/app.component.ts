07d441925223732b4ff749684f5df0b741016bec
ayman saleh <ayman.saleh000@gmail.com>
2024-05-31 16:48:57 +0300

    modyfying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,8 @@ import {UserComponent} from './user/user.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports:[HeaderComponent,  UserComponent],
+  imports: [HeaderComponent,  UserComponent],
 })
 export class AppComponent {}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,7 +10,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+
+
 export class UserComponent {
   selectUser = DUMMY_USERS[randomIndex];
 
+    get imagePath() {
+     return 'assets/users/'  + this.selectUser.avatar;
+    }
 }
