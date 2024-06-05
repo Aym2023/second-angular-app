ccf1d2235c9e9c5518f0239caca8037a93f67fc1
ayman saleh <ayman.saleh000@gmail.com>
2024-06-05 18:37:17 +0300

    modyfying

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -3,6 +3,7 @@ import { DUMMY_USERS } from '../user/dummy-users';
 import {TaskComponent} from './task/task.component';
 import {NewTaskComponent} from './new-task/new-task.component';
 import {type NewTaskData} from './task/task.model';
+import { TaskService } from './tasks.service';
 
 
 @Component({
@@ -16,39 +17,16 @@ export class TasksComponent {
   @Input({required:true}) userId!: string;
   @Input({required:true}) name!: string;
   isAddingTask = false;
-
-tasks = [
-  {
-    id: 't1',
-    userId: 'u1',
-    title: 'Master Angular',
-    summary:
-      'Learn all the basic and advanced features of Angular & how to apply them.',
-    dueDate: '2025-12-31',
-  },
-  {
-    id: 't2',
-    userId: 'u3',
-    title: 'Build first prototype',
-    summary: 'Build a first prototype of the online shop website',
-    dueDate: '2024-05-31',
-  },
-  {
-    id: 't3',
-    userId: 'u3',
-    title: 'Prepare issue template',
-    summary:
-      'Prepare and describe an issue template which will help with project management',
-    dueDate: '2024-06-15',
-  },
-]
-
-get selecedUsertask () {
-  return this.tasks.filter((task) => this.userId === task.userId )
+  // private taskService = new TaskService();
+ constructor(private tasksService : TaskService){
+ }
+ 
+  get selecedUsertask () {
+  return this.tasksService.getUsertasks(this.userId);
 }
 
 onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+   return  ;
 }
 
 onStartAddTAsk () {
@@ -60,13 +38,7 @@ onCancelAddTask () {
 }
 
 onAddTask(newdata: NewTaskData) {
-  this.tasks.unshift({
-    id: new Date().getTime().toString(),
-    userId: this.userId,
-    title: newdata.title,
-    summary: newdata.summary,
-    dueDate: newdata.date,
-  })
+  return;
 
  this.isAddingTask = false;
 }
